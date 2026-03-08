815862326c545f6297123220bfb160f1af76d578
pjdevs <pj33720@gmail.com>
2026-03-08 16:02:01 +0100

    test: add some useful testing package for nice output

--- tests/test_god_tier_app.py
+++ tests/test_god_tier_app.py
@@ -1,4 +1,7 @@
+from dirty_equals import IsStr
 from fastapi.testclient import TestClient
+from hypothesis import given, settings
+from hypothesis import strategies as st
 from pytest_mock import MockerFixture
 
 from god_tier_app.app import app, container
@@ -9,7 +12,15 @@ client = TestClient(app)
 
 async def test_hello():
     result = await hello()
-    assert result.startswith("Hello")
+    assert result == IsStr(regex=r"^Hello.*")
+
+
+@given(st.text(min_size=1, max_size=20))
+@settings(max_examples=10)
+async def test_super_greeter(name: str):
+    greeter = SuperGreeter()
+    result = await greeter.greet(name)
+    assert result == f"Hello, {name}!"
 
 
 async def test_index():
@@ -20,17 +31,19 @@ async def test_index():
 
 async def test_greet(mocker: MockerFixture):
     # Arrange
+    greeting = "Fake greeting"
+    name = "World"
     mock_greeter = mocker.AsyncMock(spec=SuperGreeter)
-    mock_greeter.greet.return_value = "Greet"
+    mock_greeter.greet.return_value = greeting
 
     # Make HTTP call
     with container.override.injectable(target=SuperGreeter, new=mock_greeter):
-        response = client.get("/greet/World")
+        response = client.get(f"/greet/{name}")
 
     # Assert HTTP
     assert response.status_code == 200
-    assert response.json() == "Greet"
+    assert response.json() == greeting
 
     # Assert internal calls
-    mock_greeter.greet.assert_called_once_with("World")
+    mock_greeter.greet.assert_called_once_with(name)
     mock_greeter.greet.assert_awaited()
